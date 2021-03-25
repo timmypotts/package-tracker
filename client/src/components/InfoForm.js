6f914b457cbacb176092feca673fd486dcc105a9
Tim <timothy.potts@colorado.edu>
2021-03-25 07:40:11 -0600

    got it rolling

--- client/src/components/InfoForm.js
+++ client/src/components/InfoForm.js
@@ -1,52 +1,82 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import InputBase from "@material-ui/core/InputBase";
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    "label + &": {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    padding: "10px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+    },
+  },
+}))(InputBase);
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& .MuiTextField-root": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
+  margin: {
+    margin: theme.spacing(1),
   },
 }));
 
-export default function MultilineTextFields() {
+export default function CustomizedSelects() {
   const classes = useStyles();
-  const [value, setValue] = React.useState("Controlled");
-
+  const [item, setItem] = React.useState("");
   const handleChange = (event) => {
-    setValue(event.target.value);
+    setItem(event.target.value);
   };
-
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="standard"
-          label="Item"
-          multiline
-          rowsMax={1}
-          value={value}
-          onChange={handleChange}
-        />
-        <TextField
-          id="standard"
-          label="Tracking Number"
-          multiline
-          rowsMax={1}
-          value={value}
-          onChange={handleChange}
-        />
-        <TextField
-          id="standard"
-          label="Courier"
-          multiline
-          rowsMax={1}
-          value={value}
-          onChange={handleChange}
-        />
-      </div>
-    </form>
+    <div>
+      <FormControl className={classes.margin}>
+        <InputLabel htmlFor="demo-customized-textbox">Item Name</InputLabel>
+        <BootstrapInput id="demo-customized-textbox" />
+      </FormControl>
+      <FormControl className={classes.margin}>
+        <InputLabel htmlFor="demo-customized-textbox">
+          Tracking Number
+        </InputLabel>
+        <BootstrapInput id="demo-customized-textbox" />
+      </FormControl>
+      <FormControl style={{ minWidth: 100 }} className={classes.margin}>
+        <InputLabel>Courier</InputLabel>
+        <Select value={item} onChange={handleChange} input={<BootstrapInput />}>
+          <MenuItem value="">
+            <em>Courier</em>
+          </MenuItem>
+          <MenuItem value={"USPS"}>USPS</MenuItem>
+          <MenuItem value={"UPS"}>UPS</MenuItem>
+          <MenuItem value={"FedEx"}>FedEx</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
   );
 }
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,21 +2,47 @@ import React, { useState } from "react";
 import InfoForm from "../InfoForm";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  title: {
+    fontWeight: "bold",
+  },
+});
 
 export default function Home() {
+  const classes = useStyles();
+  const [view, setView] = useState(false);
+
+  const toggle = () => {
+    if (view === false) {
+      setView(true);
+    } else {
+      setView(false);
+    }
+  };
   return (
     <Container maxWidth="lg">
       <header>
-        <h1>Trackage</h1>
+        <Typography className={classes.title} variant="h2">
+          Trackage
+        </Typography>
         <h3>
-          Keep track of all your packages without having to scour your inbox for
-          that one email with the tracking number.
+          Keep track of all your packages without having to repeatedly scour
+          your inbox for that one email with the tracking number.
         </h3>
       </header>
-      <Button variant="contained" color="secondary">
+      <Button
+        size="large"
+        variant="contained"
+        onClick={toggle}
+        color="secondary"
+      >
         + Track a Package
       </Button>
-      <InfoForm />
+      <br />
+      {view ? <InfoForm /> : null}
     </Container>
   );
 }
