3b7d1e27c96d2bd7f4500f93457ab9040b0a972d
Tim <timothy.potts@colorado.edu>
2021-04-28 23:47:49 -0600

    removed mycache

---
+++ server/track.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+key = os.environ.get("SHIPENGINE_API_KEY")
+
+
+def track(tracking, carrier):
+    carrier = carrier.lower()
+
+    url = "https://api.shipengine.com/v1/tracking?carrier_code=" + carrier + "&tracking_number=" + tracking
+
+    payload = {}
+    headers = {
+    'Host': 'api.shipengine.com',
+    'API-Key': key
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+
+
+    res = response.text.encode('utf8')
+    json_obj = json.loads(res)
+    print(json_obj)
+    return json_obj
